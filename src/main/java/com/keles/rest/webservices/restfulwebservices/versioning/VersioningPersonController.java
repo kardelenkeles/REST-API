581fe3d67f145a831bd0bdf847ddb78e821be36a
kardelenkeles <kar_di44@hotmail.com>
2023-05-28 17:49:01 +0300

    accept & header

--- src/main/java/com/keles/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/keles/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -34,5 +34,45 @@ public class VersioningPersonController {
         return person;
     }
 
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader(){
+        Name name = new Name();
+        name.setFirstName("Squidward Tentacles");
+        PersonV1 person = new PersonV1();
+        person.setName(name);
+        return person;
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV1 getSecondVersionOfPersonRequestHeader(){
+        Name name = new Name();
+        name.setFirstName("Sandy");
+        name.setLastName("Cheeks");
+
+        PersonV1 person = new PersonV1();
+        person.setName(name);
+        return person;
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader(){
+        Name name = new Name();
+        name.setFirstName("Squidward Tentacles");
+        PersonV1 person = new PersonV1();
+        person.setName(name);
+        return person;
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV1 getSecondVersionOfPersonAcceptHeader(){
+        Name name = new Name();
+        name.setFirstName("Sandy");
+        name.setLastName("Cheeks");
+
+        PersonV1 person = new PersonV1();
+        person.setName(name);
+        return person;
+    }
+
 
 }
